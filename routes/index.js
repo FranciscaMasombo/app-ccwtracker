9c2503c6338bb6e1795b519a743557beb4b5a33e
Francisca masombo <frmm97@gmail.com>
2018-10-23 17:14:21 +0100

    Users

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,25 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.post('/register',function (req, res){
+  var username = req.body.username;
+    var password = req.body.password;
+  var firstname =  req.body.firstname;
+  var lastname = req.body.lastname;
+
+  var newuser = new User();
+  newuser.username= username;
+  newuser.password= password;
+  newuser.firstname= firstname;
+  newuser.lastname = lastname;
+  newuser.save(function (err, saveUser) {
+      if(err){
+        console.log(err);
+        return res.status(500).send();
+      }
+      return res.status(200).send();
+  })
+
+});
 
 module.exports = router;
