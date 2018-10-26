51396980c26aa7de2abf7da4014ed9e5c4c68664
Francisca masombo <frmm97@gmail.com>
2018-10-26 13:52:36 +0100

    Creating and persisting a Login session Password hashing using Mongoose

---
+++ models/submissions.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose');
+let wwtdbschema =  new mongoose.Schema({
+        name: { type:String, required: true},
+        age: { type: Number, min: 1, max: 65, required: true },
+        gender: { type:String, required: true},
+        startWeight: { type:Number, required: true },
+        goalWeight:{ type:Number, required: true },
+        currentWeight:{ type:Number, required: true},
+        Height:{ type:Number},
+        location:{ type:String, required: true },
+        date: { type: Date, default: Date.now, },
+
+    }, {collection: 'wwtdb'});
+module.exports= mongoose.model('sub', wwtdbschema);
